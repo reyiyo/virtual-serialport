a5d6a2a5195d06a872e31d89f9df4ae4880a57fa
Linus Wallgren <linus.wallgren@scypho.com>
2015-03-16 15:37:26 +0100

    Add write,pause,resume,flush,drain and close
    All of which are methods exposed by Serialport

--- index.js
+++ index.js
@@ -30,6 +30,28 @@ VirtualSerialPort.prototype.open = function open(callback) {
 
 VirtualSerialPort.prototype.write = function write(buffer, callback) {
     if (this.open) this.emit("dataToDevice", buffer);
+    // This callback should receive both an error and result, however result is
+    // undocumented so I do not know what it should contain
+    callback();
+};
+
+VirtualSerialPort.prototype.pause = function pause() {
+};
+
+VirtualSerialPort.prototype.resume = function resume() {
+};
+
+VirtualSerialPort.prototype.flush = function flush(callback) {
+    callback();
+};
+
+VirtualSerialPort.prototype.drain = function drain(callback) {
+    callback();
+};
+
+VirtualSerialPort.prototype.close = function close(callback) {
+    this.removeAllListeners();
+    callback();
 };
 
 module.exports = VirtualSerialPort;
